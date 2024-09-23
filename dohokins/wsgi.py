a5930dcc6d31800e34c69b2dec9d4908c3aacde1
bamjun <khy5116@naver.com>
2024-09-23 22:59:14 +0900

    format

--- dohokins/wsgi.py
+++ dohokins/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dohokins.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dohokins.settings")
 
 application = get_wsgi_application()
